11d9174de5d322635e7b76cce49dd3ab80fba9b2
Stepan Oskin <stepanoskin007@gmail.com>
2019-07-28 12:09:49 -0400

    add key and referential constraints to IMDb table definition

--- sql/DDL/imdb_tables.sql
+++ sql/DDL/imdb_tables.sql
@@ -1,8 +1,10 @@
 /* ------------------------- name.basics table ------------------------ */
 -- auto-generated definition
-create table "name.basics"
+create table name_basics
 (
-    nconst            varchar(10) not null,
+    nconst            varchar(10) not null
+        constraint "name.basics_pk"
+            primary key,
     primaryname       varchar(120),
     birthyear         integer,
     deathyear         integer,
@@ -10,34 +12,37 @@ create table "name.basics"
     knownfortitles    varchar(100)
 );
 
-alter table "name.basics"
+alter table name_basics
     owner to movies;
-
-create unique index "name.basics_nconst_uindex"
-    on "name.basics" (nconst);
 
 /* ------------------------- title.akas table ------------------------ */
 -- auto-generated definition
-create table "title.akas"
+create table title_akas_ri
 (
-    titleid         varchar(10) not null,
+    titleid         varchar(10) not null
+        constraint "title.akas_ri_title.basics_tconst_fk"
+            references title_basics,
     ordering        integer     not null,
     title           text,
     region          varchar(4),
     language        varchar(3),
     types           varchar(20),
     attributes      varchar(80),
-    isoriginaltitle boolean
+    isoriginaltitle boolean,
+    constraint "title.akas_ri.pk"
+        primary key (titleid, ordering)
 );
 
-alter table "title.akas"
+alter table title_akas_ri
     owner to movies;
 
 /* ------------------------- title.basics table ------------------------ */
 -- auto-generated definition
-create table "title.basics"
+create table title_basics
 (
-    tconst         varchar(10) not null,
+    tconst         varchar(10) not null
+        constraint "title.basics_pk"
+            primary key,
     titletype      varchar(12),
     primarytitle   varchar(450),
     originaltitle  varchar(450),
@@ -48,70 +53,78 @@ create table "title.basics"
     genres         varchar(40)
 );
 
-alter table "title.basics"
+alter table title_basics
     owner to movies;
-
-create unique index "title.basics_tconst_uindex"
-    on "title.basics" (tconst);
 
 /* ------------------------- title.crew table ------------------------ */
 -- auto-generated definition
-create table "title.crew"
+create table title_crew_ri
 (
-    tconst    varchar(10) not null,
+    tconst    varchar(10) not null
+        constraint "title.crew_ri_pk"
+            primary key
+        constraint "title.crew_ri_title.basics_tconst_fk"
+            references title_basics,
     directors text,
     writers   text
 );
 
-alter table "title.crew"
+alter table title_crew_ri
     owner to movies;
-
-create unique index "title.crew_tconst_uindex"
-    on "title.crew" (tconst);
 
 /* ------------------------- title.episode table ------------------------ */
 -- auto-generated definition
-create table "title.episode"
+create table title_episode_ri
 (
-    tconst        varchar(10) not null,
-    parenttconst  varchar(10),
+    tconst        varchar(10) not null
+        constraint "title.episode_ri_pk"
+            primary key,
+    parenttconst  varchar(10)
+        constraint "title.episode_ri_title.basics_tconst_fk"
+            references title_basics,
     seasonnumber  integer,
     episodenumber integer
 );
 
-alter table "title.episode"
+alter table title_episode_ri
     owner to movies;
 
-create unique index "title.episode_tconst_uindex"
-    on "title.episode" (tconst);
 
 /* ------------------------- title.principals table ------------------------ */
 -- auto-generated definition
-create table "title.principals"
+create table title_principals_ri
 (
-    tconst     varchar(10) not null,
+    tconst     varchar(10) not null
+        constraint "title.principals_ri_title.basics_tconst_fk"
+            references title_basics,
     ordering   integer     not null,
-    nconst     varchar(10),
+    nconst     varchar(10) not null
+        constraint "title.principals_ri_name.basics_nconst_fk"
+            references name_basics,
     category   varchar(25),
     job        varchar(300),
-    characters varchar(500)
+    characters varchar(500),
+    constraint "title.principals_ri.pk"
+        primary key (tconst, ordering)
 );
 
-alter table "title.principals"
+alter table title_principals_ri
     owner to movies;
 
 /* ------------------------- title.ratings table ------------------------ */
 -- auto-generated definition
-create table "title.ratings"
+create table title_ratings_ri
 (
-    tconst        varchar(10) not null,
+    tconst        varchar(10) not null
+        constraint "title.ratings_ri_pk"
+            primary key
+        constraint "title.ratings_ri_title.basics_tconst_fk"
+            references title_basics,
     averagerating numeric,
     numvotes      integer
 );
 
-alter table "title.ratings"
+comment on table title_ratings_ri is 'Ratings for movie titles';
+
+alter table title_ratings_ri
     owner to movies;
-
-create unique index "title.ratings_tconst_uindex"
-    on "title.ratings" (tconst);
-
